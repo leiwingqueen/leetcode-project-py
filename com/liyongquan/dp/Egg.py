84661556e8856b6b166aadc52f2c0ca619cb1a9f
yongquanli <yongquanli@kugou.net>
2020-04-13 00:32:36 +0800

    鸡蛋问题

--- com/liyongquan/dp/Egg.py
+++ com/liyongquan/dp/Egg.py
@@ -7,17 +7,17 @@ https://leetcode-cn.com/problems/super-egg-drop/
 # searchTime(K, N) = max(searchTime(K - 1, X - 1), searchTime(K, N - X))+1
 class Solution:
     def superEggDrop(self, K: int, N: int) -> int:
-        print('开始计算...k:{0},n:{1}'.format(K, N))
         if N == 0 or K == 1 or N == 1:
             return N
-        min = N
-        for x in range(1, N + 1):
-            result = max(self.superEggDrop(K - 1, x - 1), self.superEggDrop(K, N - x)) + 1
+        min1 = N
+        for x in range(1, N+1):
+            result = max(self.superEggDrop(K - 1, x-1), self.superEggDrop(K, N - x))+1
             if result < N:
-                min = result
-        return min
+                min1 = result
+        print('计算结果...k:{0},n:{1},result:{2}'.format(K, N, min1))
+        return min1
 
 
 so = Solution()
-result = so.superEggDrop(2, 6)
-print(result)
+r = so.superEggDrop(2, 6)
+print(r)
